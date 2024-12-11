f2a0ebf7223cc116e1eee52ed73268a354ce01ba
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2024-12-11 09:01:54 +0300

    Update index.py

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ def showVehicleInfo(data, car_id=0):
                 Номер записи: {car["id"]},
                 Название модели: {car["name"]},
                 Название производителя: {car["manufacturer"]},
-                Заправляется бензином: {car["is_petrol"]},
+                Заправляется бензином: {f"да" if car["is_petrol"] else "нет"},
                 Объем бака: {car["tank_volume"]}
               """)
         else:
@@ -43,66 +43,68 @@ def showVehicleInfo(data, car_id=0):
 
 
 # Функция ввода информации о новом автомобиле.
-def inputAndCheckNewCarInfo(current_step = 1):
+def inputAndCheckNewCarInfo():
 
     # создание функции для вывода ошибки
-    def errorMassage(value, error_step):
+    def errorMessage(value):
         print(f"""
           Недопустимое значение {value},
           Повторите ввод корректно:\n""")
-        return inputAndCheckNewCarInfo(error_step)
 
-    def is_string(input_str, current_step):
+
+    def is_string(input_str):
         if isinstance(input_str, str) and (not input_str.isdigit()):
-            return
-        errorMassage(input_str, current_step)
-        return inputAndCheckNewCarInfo(current_step)
+            return True
+        errorMessage(input_str)
+        return False
+   
 
     # создание функции для проверки корректного ввода ответа
-    def checkAnswer(answer, current_step):
-        is_string(answer, current_step)
+    def checkAnswer(answer):
+        is_string(answer)
         if (answer.lower() == "да") or (answer.lower() == "нет"):
-            pass
+            return True
         else:
-            errorMassage(answer, current_step)
+            errorMessage(answer)
+            return False
 
     # создание функции для преобразования значения в float тип
-    def toFloat(input_num, current_step):
+    def toFloat(input_num):
         try:
-            return float(input_num)
+            return True, float(input_num)
         except (ValueError, TypeError):
-            errorMassage(input_num, current_step)
+            errorMessage(input_num)
+            return False, float("NaN")
 
+    current_step = 1
     new_data = []
     # Запрос данных о новом автомобиле у пользователя.
-    if current_step == 1:
-        new_name = input("Введите название модели: ")
-        new_manufacturer = input("Введите производителя: ")
-        new_is_petrol = input("Машина заправляется бензином (да/нет): ").strip()
-        checkAnswer(new_is_petrol, 3)
-        new_tank_volume = toFloat(input("Введите объём бака: "), 4)
+    while current_step < 5:
+        if current_step == 1:
+            new_name = input("Введите название модели: ")
+            new_data.append(new_name)
 
-        new_data.extend(
-            [
-                new_name, 
-                new_manufacturer,
-                new_is_petrol,
-                new_tank_volume
-            ] 
-            )
-        
-    elif current_step == 3:
-        new_is_petrol = input("Машина заправляется бензином (да/нет): ").strip()
-        checkAnswer(new_is_petrol, 3)
-        new_tank_volume = toFloat(input("Введите объём бака: "), 4)
-        new_data.extend([new_is_petrol, new_tank_volume])
+        elif current_step == 2:
+            new_manufacturer = input("Введите производителя: ")
+            new_data.append(new_manufacturer)
 
 
-    elif current_step == 4:
-        new_tank_volume = toFloat(input("Введите объём бака: "), 4)
-        new_data.append(new_tank_volume)
+        elif current_step == 3:
+            new_is_petrol = input(
+                "Машина заправляется бензином (да/нет): ").strip()
+            if not checkAnswer(new_is_petrol):
+                continue
+            new_data.append(new_is_petrol)
 
-    # Возвращает список с данными о новом автомобиле.
+        elif current_step == 4:
+            is_float, new_tank_volume = toFloat(input("Введите объём бака: "))
+            if not is_float:
+                continue
+            new_data.append(new_tank_volume)
+
+        current_step += 1
+
+        # Возвращает список с данными о новом автомобиле.
     return new_data
 
 
@@ -157,20 +159,22 @@ def output(count, actions_list, actions_count):
     # Возвращает None, чтобы не выводить лишний None в консоль
     return None
 
+
 def checkId(id):
     while not id.isdigit():
         print("Неверное значение. Введите id корректно!")
         id = input("Введите номер записи машины: ")
     return id
 
+
 # Открытие файла cars.json в режиме чтения с кодировкой utf-8.
-with open("cars.json", 'r', encoding='utf-8') as file:
+with open("ipo-lr7-4-3/cars.json", 'r', encoding='utf-8') as file:
     # Загрузка данных из файла в переменную data.
     data = json.load(file)
 
 # Инициализация счетчика выполненных операций.
 count = 0
-last_id = 5
+
 # Список доступных действий.
 actions_list = [
     "Вывести все записи",
@@ -228,6 +232,9 @@ while True:
             print("Запись не найдена.")
 
     elif num == 3:
+        find = False
+        last_id = int(data[-1].get("id")) + 1
+
         new_name, new_manufacturer, new_is_petrol, new_tank_volume = inputAndCheckNewCarInfo()
         # Создание нового автомобиля и добавление его в список.
         new_car = createNewCar(
@@ -236,13 +243,11 @@ while True:
         # Сохранение изменений в файл.
         with open("cars.json", 'w', encoding='utf-8') as out_file:
             json.dump(data, out_file)
+
         print("Машина успешно добавлена.")
-        
-            
 
         # Увеличение счетчиков.
         count += 1
-        last_id += 1
         actions_count[3] += 1
 
     elif num == 4:
@@ -276,3 +281,4 @@ while True:
     # Обработка некорректного ввода.
     else:
         print("Такого номера нет.")
+
